74d03db123b439f74d94ce34cac1a4553651d5a8
Davi Chechetto <davichechetto@gmail.com>
2024-06-19 21:01:35 -0300

    Verificação se o usuario existe

--- Cliente.py
+++ Cliente.py
@@ -2,13 +2,25 @@ import getpass
 from Admin import D
 
 def Cliente():
-    usuario = input("Digite o seu usuário: ")
-    senha = getpass.getpass("Digite a sua senha: ")
+    
+    while True:
+        usuario = input("Digite o seu usuário: ")
+        senha = getpass.getpass("Digite a sua senha: ")
 
-    if D[usuario] and D.get(usuario):
-        print()
+        # Procura se o usuario está cadastrado
+        for i in D:
+           
+            if D[i][0] == usuario:
+                print("usuario encontrado!")
 
-    opcao = int(input("1 - Depositar\n2 - Sacar\n3 - Voltar\nDigite qm vc é: "))
+                #Usuário Existe  &&  verificação se a senha está correta
+                if D[i][1] == senha:
+                    print("senha correta!")
+            
+                    opcao = int(input("1 - Depositar\n2 - Sacar\n3 - Voltar\nDigite a opção desejada: "))
 
-    depositar =  float(input("Digite um nome: "))
+                    if opcao == 1:
+                        depositar =  float(input("Digite um nome: "))
+        
+        print("usuario não encontrado!")
 
--- main.py
+++ main.py
@@ -5,47 +5,13 @@ while True:
     nivel = int(input("1 - Administrador\n2 - Cliente\n3 - Sair\nDigite qm vc é: "))
     if nivel == 1:
         Admin()
-        break
     elif nivel == 2:
         Cliente()
-        break
     elif nivel == 3:
         print("Saiu!")
         break
     else:
-        print("Erro, operador inválido!")
+        print("Erro, operador inválido!") 
     
 
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-""" for i in range(1):
-    nome = input("Digite seu nome e sobrenome: ")
-    cpf = input("Digite seu numero de cpf: ")
-    chave = input("Digite uma chave de cadastro: ")
-    D.update({(nome , cpf):chave})
-print(f'Agradecemos pela confiança {nome}, sua conta já foi criada e está pronta para uso.' ) """
+print("Fim do programa!")
